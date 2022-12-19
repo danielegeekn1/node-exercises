20f11adf8cd0efe065dea4ffc38992946c7afb50
Daniele Basile <daniele.basile.job@gmail.com>
2022-12-19 16:59:27 +0100

    writefile in callback ex done

---
+++ app.js
@@ -0,0 +1,14 @@
+let http = require("http");
+const { createServer } = require("tls");
+function createApp() {
+  return createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    // res.setHeader("Content-Type", "application/json");
+    // const jsonBody = JSON.stringify({ location: "Earth" });
+    res.end("Welcome to the World Wide Web");
+  });
+}
+server.lister(3000, () => {
+  console.log("listening to the server");
+});
