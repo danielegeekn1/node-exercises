d3204b0db1e02f2b4f8600ce14512f27b5678958
Daniele Basile <daniele.basile.job@gmail.com>
2022-12-19 14:07:11 +0100

    ecma-script-module ex done

---
+++ message.mjs
@@ -0,0 +1,4 @@
+function outputMsg(msg) {
+  console.log(`The output message is:${msg}`);
+}
+export default outputMsg;
---
+++ outputMsg.mjs
@@ -0,0 +1,2 @@
+import outputMsg from "./message.mjs";
+outputMsg("One Piece is the best manga ever");
